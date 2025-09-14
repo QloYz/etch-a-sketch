2889b3064a4624493213c0fbb26d2838a835b9a0
Henry Grant <henryhpgv5@gmail.com>
2025-09-14 10:46:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# ETCH-A-SKETCH #
+
+A browser-based Etch-A-Sketch drawing application built with HTML, CSS, and JavaScript. Users can draw on a dynamic grid, change brush colors, and clear the drawing — just like a classic Etch-A-Sketch toy.
+
+# FEATURES #
+
+Draw on grid of hovering squares
+Change brush color with buttons
+Clear grid at any time
+Clear individual boxes with eraser
+Adjust brush size dynamically with slider (in Progress)
+
+# Technologies Used #
+HTML5 - structure & layout
+CSS - Styling & flexbox layout, hover effects
+JavaScript - Logic & backend
+
+# Future Improvements #
+Fix Slider (MOST IMPORTANT and Currently working on)
+Additional Brush colors
+Animated technocolor effect
+Save/Load Drawings
+
+# Credits #
+Developed by Henry Grant
+Inspired by Classic Etch-A-Sketch Toy
+Project idea through the Odin Project
---
+++ script.js
@@ -0,0 +1,89 @@
+const container = document.querySelector(".flex-container");
+const redButton = document.querySelector("#red");
+const blueButton = document.querySelector("#blue")
+const greenButton = document.querySelector("#green")
+const purpleButton = document.querySelector("#purple")
+const yellowButton = document.querySelector("#yellow")
+const eraseButton = document.querySelector("#erase")
+const clearButton = document.querySelector("#clear")
+const slider = document.getElementById("slider")
+const sizeValue = document.getElementById("sliderValue")
+const size = 16;
+let isDrawing = false;
+let brushColor = "black";
+
+for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div")
+    square.classList.add("box")
+    container.appendChild(square)
+}
+
+
+
+redButton.addEventListener('click', () => {
+    brushColor = "red"
+})
+
+blueButton.addEventListener('click', () => {
+    brushColor = "blue"
+})
+
+greenButton.addEventListener('click', () => {
+    brushColor = "green"
+})
+
+purpleButton.addEventListener('click', () => {
+    brushColor = "purple"
+})
+
+yellowButton.addEventListener("click", () => {
+    brushColor = "yellow"
+})
+
+eraseButton.addEventListener("click", () => {
+    brushColor = "white"
+})
+
+clearButton.addEventListener("click", () => {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach(box => {
+    box.style.backgroundColor = "white";
+  });
+});
+
+container.addEventListener("click", (e) => {
+    if (e.target.classList.contains("box")) {
+        isDrawing = !isDrawing;
+    }
+})
+
+container.addEventListener("mouseover", (e) => {
+    if (isDrawing && (e.target.classList.contains("box"))) {
+        e.target.style.backgroundColor = brushColor
+    }
+})
+
+function titleRainbow() {
+  const title = document.getElementById("title");
+  const text = title.textContent;
+  title.textContent = "";
+
+  const colors = [
+    "red", "orange", "yellow", "green", 
+    "blue", "indigo", "violet"
+  ];
+
+  for (let i = 0; i < text.length; i++) {
+    const span = document.createElement("span");
+    span.textContent = text[i];
+    span.style.color = colors[i % colors.length];
+    title.appendChild(span);
+  }
+}
+
+titleRainbow();
+
+
+
+
+
